c24f5708961d6d59c7092c2fe3640e305831a4a5
johonnes <167844970+johonnes@users.noreply.github.com>
2024-06-02 04:16:49 +0000

    corrigindo o caminho do arquivo teste

--- 05 - Manipulação de arquivos/2_operacao_escrita.py
+++ 05 - Manipulação de arquivos/2_operacao_escrita.py
@@ -1,5 +1,5 @@
 arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
+    "/workspaces/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
 )
 arquivo.write("Escrevendo dados em um novo arquivo.")
 arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
