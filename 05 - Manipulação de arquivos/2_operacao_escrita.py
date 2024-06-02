e8eb5e9dda5e061e1c28c622836714f718a05265
johonnes <167844970+johonnes@users.noreply.github.com>
2024-06-02 04:38:02 +0000

    brincando com o codigo

--- 05 - Manipulação de arquivos/2_operacao_escrita.py
+++ 05 - Manipulação de arquivos/2_operacao_escrita.py
@@ -1,5 +1,5 @@
 arquivo = open(
-    "/workspaces/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
+    "/workspaces/trilha-python-dio/05 - Manipulação de arquivos/novo.txt", "w"
 )
 arquivo.write("Escrevendo dados em um novo arquivo.")
 arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
